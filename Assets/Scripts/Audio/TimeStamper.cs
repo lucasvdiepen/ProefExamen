ff25d2a2655966b2da6ce7a32ea24ae73863707b
PiterGrootHOME <piter.groot1104@gmail.com>
2024-02-28 15:48:54 +0100

    Started working being able to select and tweak specific time stamps

--- Assets/Scripts/Audio/TimeStamper.cs
+++ Assets/Scripts/Audio/TimeStamper.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TimeStamper : MonoBehaviour
 {
     [SerializeField] private Color _timeStampColor = Color.blue;
+    [SerializeField] private Color _selectedTimeStampColor = Color.yellow;
     [SerializeField] private float _stampLineHeightReduction = 100;
     [SerializeField] private float _gizmoOffset = .25f;
 
@@ -15,13 +16,15 @@ public class TimeStamper : MonoBehaviour
     [SerializeField] private List<TimeStampData> _timeStamps = new List<TimeStampData>();
     
     private AudioWaveformDrawer _waveformDrawer = null;
+    private TimeStampData _currentSelectedTimeStamp;
 
     [System.Serializable]
-    public struct TimeStampData
+    public class TimeStampData
     {
         public Vector2 startPointPosition;
         public Vector2 endPointPosition;
         public float songTime;
+        public bool isSelected = false;
 
         public TimeStampData(Vector2 start, Vector2 end, float time)
         {
@@ -55,17 +58,56 @@ public class TimeStamper : MonoBehaviour
             if(_timeStamps.Count > 0)
                 _timeStamps.RemoveAt(_timeStamps.Count - 1);
         }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TimeStampData closestStampToMouse = GetClosestTimeStamp(mousePosition);
+
+            if(closestStampToMouse != _currentSelectedTimeStamp)
+            {
+                closestStampToMouse.isSelected = true;
+                if(_currentSelectedTimeStamp != null)
+                    _currentSelectedTimeStamp.isSelected = false;
+                _currentSelectedTimeStamp = closestStampToMouse;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            _currentSelectedTimeStamp.isSelected = false;
+            _currentSelectedTimeStamp = null;
+        }
+    }
+
+    TimeStampData GetClosestTimeStamp(Vector2 originPosition)
+    {
+        TimeStampData bestTarget = default;
+        float closestDistanceSqr = Mathf.Infinity;
+        foreach (TimeStampData timeStampData in _timeStamps)
+        {
+            Vector3 directionToTarget = timeStampData.startPointPosition - originPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = timeStampData;
+            }
+        }
+
+        return bestTarget;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = _timeStampColor;
         for (int i = 0; i < _timeStamps.Count; i++)
         {
             //Little hacky but SOMETIME I HATE UNITY, you can't set the thickness of the gizmos line. 
             //This fixes the gizmo flickering when it's ony 1px wide.
 
+            Gizmos.color = _timeStamps[i].isSelected ? _selectedTimeStampColor : _timeStampColor;
             Vector2 offset = new Vector2(_gizmoOffset, 0);
+
             Gizmos.DrawLine(_timeStamps[i].startPointPosition, _timeStamps[i].endPointPosition); //center
             Gizmos.DrawLine(_timeStamps[i].startPointPosition - offset, _timeStamps[i].endPointPosition - offset); //left
             Gizmos.DrawLine(_timeStamps[i].startPointPosition + offset, _timeStamps[i].endPointPosition + offset); //right
