133e575caee5a2f06b1cde2c006b2814954330b3
PiterGrootHOME <piter.groot1104@gmail.com>
2024-03-12 10:07:37 +0100

    Added play sound method

--- Assets/Scripts/Framework/Audio/AudioSystem.cs
+++ Assets/Scripts/Framework/Audio/AudioSystem.cs
@@ -1,27 +1,62 @@
-using ProefExamen.Framework.Utils;
+using Random = UnityEngine.Random;
+using DG.Tweening;
 using UnityEngine;
+using System;
+
+using ProefExamen.Framework.Utils;
 
 namespace ProefExamen.Framework.Audio
 {
     /// <summary>
     /// Class responsible for managing general audio effect
     /// </summary>
-    [RequireComponent(typeof(AudioSource))] 
     public class AudioSystem : AbstractSingleton<AudioSystem>
     {
-        [Header("UI Sounds"), SerializeField]
-        private AudioCollectionContainer[] _audioCollectionClips;
+        [Header("Sounds"), SerializeField]
+        private AudioCollection[] _audioCollections;
 
-        private AudioSource _audioSource;
+        private int _soundLenghtCounter = 0;
 
-        private void Awake()
+        private void Update()
         {
-            _audioSource = GetComponent<AudioSource>();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayRandomSoundEffect();
+            }
         }
 
         public void PlayRandomSoundEffect()
         {
-            //_audioSource.clip = _audioCollectionClips[Random.Range(0, _audioCollectionClips.Length)].clips[Random.Range(0, ;
+            Array availableAudioTypes = Enum.GetValues(typeof(AudioCollectionTypes));   
+            int randIndex = Random.Range(0, availableAudioTypes.Length);
+
+            PlayRandomSoundEffectOfType((AudioCollectionTypes)availableAudioTypes.GetValue(randIndex));
+        }
+
+        public void PlayRandomSoundEffectOfType(AudioCollectionTypes audioCollectionType, float volume = .75f)
+        {
+            for (int i = 0; i < _audioCollections.Length; i++)
+            {
+                if (_audioCollections[i].audioCollectionType != audioCollectionType)
+                    continue;
+
+                int randIndex = Random.Range(0, _audioCollections[i].audioClipList.Count);
+                PlaySound(_audioCollections[i].audioClipList[randIndex], volume);
+            }
+        }
+
+        private void PlaySound(AudioClip clip, float volume = .75f, bool fadeIn = true)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+
+            source.clip = clip;
+            source.volume = volume;
+
+            source.Play();
+
+            float audioClipLenght = clip.length;
+            DOTween.To(() => _soundLenghtCounter, x => _soundLenghtCounter = x, 1, audioClipLenght)
+                .OnComplete(() => Destroy(source));
         }
     }
 }
