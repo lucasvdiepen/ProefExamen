5e4d1d210b8db33c835916ec1bd581c9aefb3528
PiterGrootHOME <piter.groot1104@gmail.com>
2024-03-13 14:42:31 +0100

    Added model rotation for hit animations

--- Assets/Scripts/Framework/Character/CharacterAnimationController.cs
+++ Assets/Scripts/Framework/Character/CharacterAnimationController.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 
 using ProefExamen.Framework.Gameplay.LaneSystem;
+using DG.Tweening;
 
 namespace ProefExamen.Framework.Character
 {
     /// <summary>
     /// Class responsible for controlling the charater animations.
     /// </summary>
-    [RequireComponent(typeof(Animator))]
     public class CharacterAnimationController : MonoBehaviour
     {
+
+        [SerializeField]
+        private float _rotateAmount = 10;
+
+        [SerializeField]
+        private float _rotateDuration = .35f;
+
         private Animator _characterAnim = null;
 
         private int _leftHitHash = 0;
@@ -17,7 +24,7 @@ namespace ProefExamen.Framework.Character
 
         private void Awake()
         {
-            _characterAnim = GetComponent<Animator>();
+            _characterAnim = GetComponentInChildren<Animator>();
 
             _leftHitHash = Animator.StringToHash("HitLeft");
             _rightHitHash = Animator.StringToHash("HitRight");
@@ -35,6 +42,11 @@ namespace ProefExamen.Framework.Character
 
             bool hitLeftLane = laneID == 0 || laneID == 1;
             _characterAnim.SetTrigger(hitLeftLane ? _leftHitHash : _rightHitHash);
+
+            // Rotate character body based on hit laneID.
+            if (laneID == 0) transform.DORotate(new Vector3(0, -_rotateAmount, 0), _rotateDuration);
+            else if (laneID == 3) transform.DORotate(new Vector3(0, _rotateAmount, 0), _rotateDuration);
+            else transform.DORotate(Vector3.zero, _rotateDuration);
         }
 
         private void OnDisable()
